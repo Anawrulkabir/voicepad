82eff39fd70b4713bf7b8734646435a2b37d4f17
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-12-23 21:08:08 +0600

    Some changes added

--- src/app/(routes)/(homepage)/page.tsx
+++ src/app/(routes)/(homepage)/page.tsx
@@ -1,7 +1,6 @@
 import AskRecordChatBtn from '@/components/ask-record-chat'
 import LandingPage2 from '@/components/landing-page/design2'
 import ScrollProgress from '@/components/ui/scroll-progress'
-import { HydrationOverlay } from '@builder.io/react-hydration-overlay'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
@@ -17,23 +16,7 @@ const ServerComponent2 = dynamic(
 const page = () => {
   return (
     <>
-      {/* <nav className="flex justify-between items-center p-4 bg-gray-100  sticky top-0 z-10">
-        <div className="font-mono font-bold text-xl text-black">Voice Pad</div>
-        <div className="text-zinc-600">Search Anything...</div>
-        <div>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      </nav>
-      <AskRecordChatBtn /> */}
-
       <ScrollProgress className="top-0" />
-
-      {/* <LandingPage2 /> */}
 
       <ServerComponent1 />
     </>
